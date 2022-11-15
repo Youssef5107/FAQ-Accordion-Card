24c40512f998e97a1acdb8216975939290c235d8
Youssef <ybelalmarzouk@gmail.com>
2022-11-15 22:23:57 +0200

    feut:add show function

---
+++ javascript/javascript.js
@@ -0,0 +1,29 @@
+var content2 = document.querySelector(".content2");
+var content3 = document.querySelector(".content3");
+var content4 = document.querySelector(".content4");
+var content5 = document.querySelector(".content5");
+
+function show1() {
+	var content1 = document.querySelector(".content1");
+	content1.style.display = "block";
+}
+
+function show2() {
+	var content1 = document.querySelector(".content2");
+	content1.style.display = "block";
+}
+
+function show3() {
+	var content1 = document.querySelector(".content3");
+	content1.style.display = "block";
+}
+
+function show4() {
+	var content1 = document.querySelector(".content4");
+	content1.style.display = "block";
+}
+
+function show5() {
+	var content1 = document.querySelector(".content5");
+	content1.style.display = "block";
+}
