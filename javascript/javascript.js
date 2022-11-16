93d8347cf7f9472e2d8f26aaf13ff487a3a617c8
Youssef <ybelalmarzouk@gmail.com>
2022-11-16 22:56:19 +0200

    feut:add toggel function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,17 @@
 function show(divCls) {
 	var content = document.querySelector("." + divCls);
+	for (i = 0; i < divCls.lenght; i++) {
+		divCls[i].style.display = "none";
+	}
 
-	if (content)
+	if (content.style.display == "block") {
+		content.style.display = "none";
+	}
+	else {
 		content.style.display = "block";
+	}
+
+
 }
+
+
