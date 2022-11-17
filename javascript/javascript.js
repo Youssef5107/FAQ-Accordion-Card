7f76442c2b05c9b5c404ef192fe29a40b7614768
Youssef <ybelalmarzouk@gmail.com>
2022-11-17 20:23:40 +0200

    style: Suport accordion style

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,16 +1,17 @@
 function show(divCls) {
-	var arrows = document.querySelectorAll("arrow")
-	var content = document.querySelector("." + divCls);
-	for (i = 0; i < divCls.lenght; i++) {
-		content.style.display = "none";
+	var allContents = document.querySelectorAll(".content");
+	for (i = 0; i < allContents.length; i++) {
+		allContents[i].style.display = "none";
 	}
 
+	var arrows = document.querySelectorAll(".arrow");
+	var content = document.querySelector("." + divCls);
 	if (content.style.display == "block") {
 		content.style.display = "none";
 	}
 	else {
 		content.style.display = "block";
-		arrows.style.transform = "rotate(180deg)";
+		// arrows.style.transform = "rotate(180deg)";
 	}
 
 
