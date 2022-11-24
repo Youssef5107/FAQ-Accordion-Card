caf844f12c5583090bb81fbb5a9776c96efb9eff
Youssef <ybelalmarzouk@gmail.com>
2022-11-24 23:10:36 +0200

    feat:adjust javascript function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -21,8 +21,14 @@
 
 function add_removeclass(clickedItem) {
 	var contents = document.querySelectorAll(".main-idea");
-	for ()
+	for (i = 0; i < contents.length; i++) {
+		contents[i].classList.remove("active");
+	}
 
+	if (clickedItem.classList.contains("active")) {
+		clickedItem.classList.remove("active")
+	}
+	else {
 		clickedItem.classList.add("active");
-
+	}
 }
