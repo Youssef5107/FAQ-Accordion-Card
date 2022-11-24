70c05c95b83caab0c668543f67b8c44e3b8bb5d4
Youssef <ybelalmarzouk@gmail.com>
2022-11-24 22:58:38 +0200

    feat:adjust javascript function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,21 +1,28 @@
-function show(divCls, clickedItem) {
+// function show(divCls, clickedItem) {
 
-	var allContents = document.querySelectorAll(".content");
-	for (i = 0; i < allContents.length; i++) {
-		allContents[i].style.display = "none";
-	}
+// 	var allContents = document.querySelectorAll(".content");
+// 	for (i = 0; i < allContents.length; i++) {
+// 		allContents[i].style.display = "none";
+// 	}
+
+// 	var arrow1 = document.querySelector(".arrow1")
+// 	var arrows = document.querySelectorAll(".arrow");
+// 	var content = document.querySelector("." + divCls);
+// 	if (content.style.display == "block") {
+// 		content.style.display = "none";
+// 	}
+// 	else {
+// 		content.style.display = "block";
+// 		arrow1.style.transform = "rotate(180deg)";
+// 	}
+
+// }
 
 
-	var arrows = document.querySelectorAll(".arrow");
-	var content = document.querySelector("." + divCls);
-	if (content.style.display == "block") {
-		content.style.display = "none";
-	}
-	else {
-		content.style.display = "block";
-		// arrows.style.transform = "rotate(180deg)";
-	}
+function add_removeclass(clickedItem) {
+	var contents = document.querySelectorAll(".main-idea");
+	for ()
+
+		clickedItem.classList.add("active");
 
 }
-
-
