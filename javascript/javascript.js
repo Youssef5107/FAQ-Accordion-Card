77d435b5554a12ae2d21a76d1e2c26770df2f7f9
Youssef <ybelalmarzouk@gmail.com>
2022-11-24 23:35:04 +0200

    style:add title style function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,26 +1,6 @@
-// function show(divCls, clickedItem) {
-
-// 	var allContents = document.querySelectorAll(".content");
-// 	for (i = 0; i < allContents.length; i++) {
-// 		allContents[i].style.display = "none";
-// 	}
-
-// 	var arrow1 = document.querySelector(".arrow1")
-// 	var arrows = document.querySelectorAll(".arrow");
-// 	var content = document.querySelector("." + divCls);
-// 	if (content.style.display == "block") {
-// 		content.style.display = "none";
-// 	}
-// 	else {
-// 		content.style.display = "block";
-// 		arrow1.style.transform = "rotate(180deg)";
-// 	}
-
-// }
-
-
 function add_removeclass(clickedItem) {
 	var contents = document.querySelectorAll(".main-idea");
+
 	for (i = 0; i < contents.length; i++) {
 		contents[i].classList.remove("active");
 	}
@@ -30,5 +10,6 @@ function add_removeclass(clickedItem) {
 	}
 	else {
 		clickedItem.classList.add("active");
+
 	}
 }
