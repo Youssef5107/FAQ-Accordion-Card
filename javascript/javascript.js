06914ab6c5a9345f333f15c5317aa70c6a428734
Youssef <ybelalmarzouk@gmail.com>
2022-11-27 16:02:22 +0200

    feat:add toggel

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,17 @@
 function add_removeclass(clickedItem) {
-	var contents = document.querySelectorAll(".main-idea");
 
-	for (i = 0; i < contents.length; i++) {
-		contents[i].classList.remove("active");
+	//Remove active class from other divs:
+	var contents = document.querySelectorAll(".main-idea");
+	for (let i = 0; i < contents.length; i++) {
+		if (contents[i] != clickedItem)
+			contents[i].classList.remove("active");
 	}
 
+	//toggle the active class in the clicked item
 	if (clickedItem.classList.contains("active")) {
-		clickedItem.classList.remove("active")
+		clickedItem.classList.remove("active");
 	}
 	else {
 		clickedItem.classList.add("active");
-
 	}
 }
